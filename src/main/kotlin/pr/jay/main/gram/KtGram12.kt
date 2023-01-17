30c81bf1f78154645152f774a0f1ba2b90b46be4
qkr2435 <qkr2435@gmail.com>
2023-01-17 23:52:11 +0900

    stack 구조 확인

--- src/main/kotlin/pr/jay/main/gram/KtGram12.kt
+++ src/main/kotlin/pr/jay/main/gram/KtGram12.kt
@@ -1,5 +1,8 @@
 package pr.jay.main.gram
 
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
 class KtGram12 {
 
     fun main() = with(System.`in`.bufferedReader()) {
@@ -18,6 +21,35 @@ class KtGram12 {
         //템플릿
         val s = "abc"
         println("$s.length is ${s.length}") // prints "abc.length is 3"
+
+
+        //배열
+        val arr1 = IntArray(4) { it }
+// 내용물을 전체 확인하는 코드
+        print(arr1.contentToString()) // 결과 [0, 1, 2, 3]
+
+        val arr2 = IntArray(4) { it * 2 }
+// 내용물을 전체 확인하는 코드
+        print(arr2.contentToString()) // 결과 [0, 2, 4, 6]
     }
-    출력 역
-}
+
+    val numbers = mutableListOf(1, 2, 3, 4)
+    val stack = MutableList<Int>(4) { it }
+
+// stack.push(5)
+    numbers.add(5)
+
+// stack.pop()
+    numbers.removeLast()
+
+// stack.peek()
+    numbers.last()
+
+// 비어있는지 확인
+    numbers.isEmpty()
+
+// 내용물이 있는지 확인
+    numbers.isNotEmpty()
+
+//크기
+    numbers.size
